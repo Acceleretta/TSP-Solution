53421afc88e6d84a95773ea176620f870feeaeae
Chocotabo <g.leonlanderos@ugto.mx>
2024-02-12 14:54:11 -0600

    correcion de erroes

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 from read_tsp import *
+import matplotlib.pyplot as plt
 
 # Uso del método para leer el archivo
 
 nombre_archivo = "kroA100.tsp"
+archivo_optimo = "kroA100.opt.tour"
 
 nodes = read_tsp(nombre_archivo)
+optimo = read_tsp(archivo_optimo)
+individuals = create_blocks(nodes, 200, .20, len(nodes), 5, 20)
 
-individuals = create_blocks(nodes, 50, .20, len(nodes))
+print(individuals)
 
-print("Patrones de orden repetidos entre los mejores individuos:")
 
-for order, count in individuals.items():
-    print("Orden:", order, "Repetido en:", count, "individuos")
+def plot_tsp(points, optimal_path, algorithm_path):
+    x = points[:, 1]
+    y = points[:, 2]
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x, y, color='blue', label='Puntos')
+
+    # Dibujar el camino óptimo
+    optimal_x = [x[i] for i in optimal_path]
+    optimal_y = [y[i] for i in optimal_path]
+    optimal_x.append(optimal_x[0])
+    optimal_y.append(optimal_y[0])
+    plt.plot(optimal_x, optimal_y, color='green', linestyle='-', linewidth=1, label='Camino óptimo')
+
+    # Dibujar el camino generado por el algoritmo
+    algorithm_x = [x[i] for i in algorithm_path]
+    algorithm_y = [y[i] for i in algorithm_path]
+    algorithm_x.append(algorithm_x[0])
+    algorithm_y.append(algorithm_y[0])
+    plt.plot(algorithm_x, algorithm_y, color='red', linestyle='-', linewidth=1, label='Camino generado')
+
+    plt.xlabel('Coordenada X')
+    plt.ylabel('Coordenada Y')
+    plt.title('TSP')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+plot_tsp(nodes[:, 0], optimo, individuals)
+
+# Función para graficar los puntos y el camino
--- read_tsp.py
+++ read_tsp.py
@@ -24,61 +24,101 @@ def read_tsp(archive):
     return nodos_array
 
 
-def create_blocks(nodes, population_size, selected_population, individual_size):
-    individuals, distances = create_individual(population_size, nodes, individual_size)
-    n = int(len(distances) * selected_population)
-    indexes_best_individuals = sorted(enumerate(distances), key=lambda x: x[1])[:n]
-    solutions_matrix = best_solutions_matrix(individuals, indexes_best_individuals, n)
+def create_blocks(nodes, population_size, selected_population, individual_size, selection_threshold=3,
+                  number_generations=30):
+    for i in range(number_generations):
+        if i == 0:
+            individuals, distances = create_individual(population_size, nodes, individual_size)
+        else:
+            individuals, distances = create_best_generation(population_size, nodes, individual_size, patterns,
+                                                            patterns_index)
 
-    find_order_patterns(solutions_matrix, n)
+        number_select_nodes = int(len(distances) * selected_population)
+        indexes_best_individuals = sorted(enumerate(distances), key=lambda x: x[1])[:number_select_nodes]
+        solutions_matrix = best_solutions_matrix(individuals, indexes_best_individuals, individual_size)
 
-    return repeated_orders
+        patterns, patterns_index = find_most_frecuent(solutions_matrix, number_select_nodes, selection_threshold,
+                                                      individual_size)
 
-
-
-def best_solutions_matrix(individuals, indexes_best_individuals, n):
-    best_individuals = [(individuals[index], distance) for index, distance in indexes_best_individuals]
-    solutions_matrix = []
-
-    # Access the NumPy arrays within each 'nodes' object and stack them horizontally
-    for i in range(n):
-        nodes = best_individuals[i][0]
-        solutions_matrix.append(np.hstack([node[:, 0] for node in nodes]))
-
-    return np.array(solutions_matrix)
+    return solutions_matrix[0]
 
 
-def find_order_patterns(solutions_matrix, n):
-    for i in range(n):
-        valores, frecuencia = np.unique(solutions_matrix[:, i], return_counts=True)
-        print(f"Posición {i}: {valores} - {frecuencia}")
+def best_solutions_matrix(individuals, indexes_best_individuals, individual_size):
+    best_individuals = np.zeros((len(indexes_best_individuals), individual_size))
 
-    for i in range(len(solutions_matrix[0])):
-        if solutions_matrix[0][i] == solutions_matrix[1][i] and solutions_matrix[1][i] == solutions_matrix[2][i]:
-            print(f"Patrón encontrado en la posición {i}: {solutions_matrix[0][i]}")
+    for i, (row, _) in enumerate(indexes_best_individuals):
+        best_individuals[i] = individuals[row]
+
+    return best_individuals
 
 
-def choose_random_nodes(nodos_array, n):
-    index = np.random.choice(nodos_array.shape[0], size=n, replace=False)
+def find_most_frecuent(solutions_matrix, number_select_nodes, selection_threshold, individual_size):
+    max_freq_per_column = np.zeros(individual_size)
+
+    # Calcular las frecuencias del valor más repetido por columna
+    for i in range(individual_size):
+        _, counts = np.unique(solutions_matrix[:, i], return_counts=True)
+        max_freq_per_column[i] = np.max(counts)
+
+    # Crear una máscara para identificar los patrones que cumplen con el threshold
+    patterns_mask = max_freq_per_column >= selection_threshold
+
+    # Obtener los índices de las columnas que cumplen con el threshold
+    patterns_index = np.where(patterns_mask)[0]
+
+    # Crear la lista de patrones con las columnas que cumplen con el threshold y el valor más repetido en cada una
+    patterns = [np.unique(solutions_matrix[:, i])[np.argmax(np.unique(solutions_matrix[:, i], return_counts=True)[1])]
+                for i in patterns_index]
+
+    return patterns, patterns_index
+
+
+def choose_random_nodes(nodos_array, individual_size):
+    index = np.random.choice(nodos_array.shape[0], size=individual_size, replace=False)
     random_nodes = nodos_array[index]
     return random_nodes
 
 
-def fitness(nodes):
+def fitness(individual, nodes):
     distances = []
-    for i in range(len(nodes) - 1):
-        distance = euclidean(nodes[i][1:], nodes[i + 1][1:])
+    for i in range(len(individual) - 1):
+        node1 = nodes[int(individual[i] - 1)]
+        node2 = nodes[int(individual[i + 1] - 1)]
+        distance = euclidean(node1[1:], node2[1:])
         distances.append(distance)
-        sum_distances = sum(distances)
-    return sum_distances
+    return sum(distances)
 
 
 def create_individual(population_size, nodes, individual_size):
     distances = []
-    indivuals = []
-    for _ in range(population_size):
-        individual = choose_random_nodes(nodes, individual_size)
-        indivuals.append(individual)
-        distances.append(fitness(individual))
+    individuals = np.zeros((population_size, individual_size))
 
-    return indivuals, distances
+    for i in range(population_size):
+        for j in range(individual_size):
+            available_numbers = np.setdiff1d(nodes[:, 0], individuals[i, :individual_size])
+            available_numbers = np.setdiff1d(available_numbers, individuals[i, j + 1:])
+            individuals[i, j] = np.random.choice(available_numbers)
+
+        distances.append(fitness(individuals[i], nodes))
+
+    return individuals, distances
+
+
+def create_best_generation(population_size, nodes, individual_size, patterns, patterns_index):
+    individuals = np.zeros((population_size, individual_size))
+    distances = []
+
+    for i in range(population_size):
+        # Copiar los patrones en los individuos
+        individuals[i, patterns_index] = patterns
+
+        # Verificar y llenar los números faltantes
+        for j in range(individual_size):
+            if individuals[i, j] == 0:
+                available_numbers = np.setdiff1d(nodes[:, 0], individuals[i, :individual_size])
+                available_numbers = np.setdiff1d(available_numbers, individuals[i, j + 1:])
+                individuals[i, j] = np.random.choice(available_numbers)
+
+        distances.append(fitness(individuals[i], nodes))
+
+    return individuals, distances
