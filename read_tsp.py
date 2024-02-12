6eda9fea7b0be9437864bd6acf70299265e9ebdc
Gustavo León Landeros <g.leonlanderos@ugto.mx>
2024-02-11 23:21:16 -0600

    Initialize

--- read_tsp.py
+++ read_tsp.py
@@ -25,18 +25,33 @@ def read_tsp(archive):
 
 
 def create_blocks(nodes, population_size, selected_population, individual_size):
-    distances = []
-    indivuals = []
-    for _ in range(population_size):
-        individual = choose_random_nodes(nodes, individual_size)
-        indivuals.append(individual)
-        distances.append(fitness(individual))
-
+    individuals, distances = create_individual(population_size, nodes, individual_size)
     n = int(len(distances) * selected_population)
-    indexs_best_individuals = sorted(enumerate(distances), key=lambda x: x[1])[:n]
+    indexes_best_individuals = sorted(enumerate(distances), key=lambda x: x[1])[:n]
 
-    for indice, valor in indexs_best_individuals:
-        print("Índice:", indice, "Valor:", valor)
+    best_individuals = [(individuals[index], distance) for index, distance in indexes_best_individuals]
+    #falta obtener el nombre del nodo
+    nodeName = best_individuals[0][:, 0]
+
+    repeated_orders = find_order_patterns(best_individuals)
+
+    return repeated_orders
+
+
+def find_order_patterns(best_individuals):
+    order_counts = {}
+
+    for individual, _ in best_individuals:
+        order = tuple(node[0] for node in individual)  # Tomamos solo los índices de los nodos
+        if order in order_counts:
+            order_counts[order] += 1
+        else:
+            order_counts[order] = 1
+
+    # Filtrar los órdenes que se repiten en al menos dos individuos
+    repeated_orders = {order: count for order, count in order_counts.items() if count >= 2}
+
+    return repeated_orders
 
 
 def choose_random_nodes(nodos_array, n):
@@ -52,3 +67,14 @@ def fitness(nodes):
         distances.append(distance)
         sum_distances = sum(distances)
     return sum_distances
+
+
+def create_individual(population_size, nodes, individual_size):
+    distances = []
+    indivuals = []
+    for _ in range(population_size):
+        individual = choose_random_nodes(nodes, individual_size)
+        indivuals.append(individual)
+        distances.Append(fitness(individual))
+
+    return indivuals, distances
